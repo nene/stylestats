2e820a179297e2cc96fed8efb6d3138b0919a386
Rene Saarsoo <nene@triin.net>
2014-12-18 21:26:27 +0200

    Initial commit.

---
+++ index.js
@@ -0,0 +1,26 @@
+var fs = require("fs");
+var postcss = require("postcss");
+var mustache = require("mustache");
+var Iterator = require("./lib/Iterator");
+var colorStats = require("./lib/colorStats");
+var fontStats = require("./lib/fontStats");
+
+var filename = process.argv[2];
+var templateFilename = process.argv[3];
+
+var cssSource = fs.readFileSync(filename, "utf8");
+var ast = postcss.parse(cssSource);
+var iterator = new Iterator(ast);
+
+if (templateFilename) {
+    var template = fs.readFileSync(templateFilename, "utf8");
+    var html = mustache.render(template, {
+        colorStats: colorStats(iterator),
+        fontStats: fontStats(iterator),
+    });
+    console.log(html);
+}
+else {
+    console.log(groupedFontSizes);
+}
+
