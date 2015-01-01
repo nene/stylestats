ba090a4021851d0d8eb5e53aeef1fcb95042da03
Rene Saarsoo <nene@triin.net>
2015-01-01 15:22:39 +0200

    Detect change in hash fragment and re-render the page.

--- index.js
+++ index.js
@@ -3,20 +3,34 @@ var mustache = require("mustache");
 var stats = require("./lib/stats");
 var renderCharts = require("./lib/renderCharts");
 
+var templateSource;
+function afterTemplateLoaded(callback) {
+    $.get("template.html", function(tpl) {
+        templateSource = tpl;
+        callback();
+    });
+}
+
 function getCssFilename() {
     var matches = window.location.hash.match(/#!file=(.*)/);
     return matches[1] || "samples/xrebel.css";
 }
 
-$(document).ready(function(){
+function refreshStats() {
     $.get(getCssFilename(), function(cssSource) {
-        $.get("template.html", function(templateSource) {
-            var html = mustache.render(templateSource, stats(cssSource));
+        var html = mustache.render(templateSource, stats(cssSource));
 
-            $("#content").html(html);
+        $("#content").html(html);
 
-            renderCharts();
-        });
+        renderCharts();
+    });
+}
+
+$(document).ready(function(){
+    afterTemplateLoaded(function() {
+        refreshStats();
+
+        $(window).bind('hashchange', refreshStats);
     });
 });
 
