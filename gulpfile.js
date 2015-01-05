9ae994851480023f65b36d56ac68d217d9984494
Rene Saarsoo <nene@triin.net>
2015-01-05 23:12:14 +0200

    Use watchify for faster watching of browserify task.

--- gulpfile.js
+++ gulpfile.js
@@ -1,27 +1,41 @@
 var gulp = require('gulp');
 var browserify = require('browserify');
+var watchify = require('watchify');
 var source = require('vinyl-source-stream');
 var reactify = require('reactify');
+var gutil = require('gulp-util');
+
+var IS_WATCH = false;
 
 var reactifyES6 = function(file) {
     return reactify(file, {es6: true});
 };
 
 gulp.task('browserify', function() {
-    return browserify({
-            entries:['./index.js'],
-            transform: [reactifyES6],
-            extensions: ['.jsx']
-        })
-        .bundle()
-        //Pass desired output filename to vinyl-source-stream
-        .pipe(source('stylestats.js'))
-        // Start piping stream to tasks!
-        .pipe(gulp.dest('./build/'));
+    var bundler = watchify(browserify({
+        entries:['./index.js'],
+        transform: [reactifyES6],
+        extensions: ['.jsx']
+    }));
+
+    if (IS_WATCH) {
+        bundler.on("update", rebundle);
+    }
+
+    function rebundle() {
+        console.log("Rebundling...");
+        bundler.bundle()
+            .on('error', gutil.log.bind(gutil, 'Browserify Error'))
+            .pipe(source('stylestats.js'))
+            .pipe(gulp.dest('./build/'));
+    }
+
+    return rebundle();
 });
 
-gulp.task('watch', ['browserify'], function() {
-    gulp.watch(['index.js', 'lib/**/*.js', 'lib/**/*.jsx'], ['browserify']);
+gulp.task('watch', function() {
+    IS_WATCH = true;
+    gulp.start('browserify');
 });
 
 gulp.task('default', ['browserify']);
