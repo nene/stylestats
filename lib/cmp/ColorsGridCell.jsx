54d4e692b5cf3d4fbacd14e9537d4e3613bcb624
Rene Saarsoo <nene@triin.net>
2015-01-07 18:29:11 +0200

    Improve colors grid rendering.
    Don't limit width to 300px - let it expand the whole way.
    
    Cap bubbles max size - don't allow them to grow too large.

--- lib/cmp/ColorsGridCell.jsx
+++ lib/cmp/ColorsGridCell.jsx
@@ -8,12 +8,21 @@ module.exports = React.createClass({
     displayName: "ColorsGridCell",
 
     render: function() {
+        var size = this.props.size;
+        // Cap the max size at 50px
+        if (size > 50) {
+            size = 50;
+        }
+
+        // Enable more agressive scaling when we have more that 6 x 6 grid.
+        var scaleMultiplier = size < 50 ? 2 : 1.2;
+
         var css = {
-            width: this.props.size + "px",
-            height: this.props.size + "px",
-            // Scale the bubbles up 2x, so that the largest ones
+            width: size + "px",
+            height: size + "px",
+            // Scale the bubbles up 2x or 1.2x, so that the largest ones
             // will reach up to the center of surrounding bubbles.
-            transform: "scale(" + (this.props.scale * 2) + ")",
+            transform: "scale(" + (this.props.scale * scaleMultiplier) + ")",
             // To avoid larger bubbles covering up smaller ones,
             // render the smaller ones at the top.
             zIndex: Math.floor((1 - this.props.scale)*100)+1,
